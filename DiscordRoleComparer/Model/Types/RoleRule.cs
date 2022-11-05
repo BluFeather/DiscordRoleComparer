0d0cd41f079161c689790837bc508ab1dba1cb3a
BluFeather <BluFeather@protonmail.com>
2022-11-05 15:42:12 -0700

    Added Rules and RoleRule classes.

---
+++ DiscordRoleComparer/Model/Types/RoleRule.cs
@@ -0,0 +1,16 @@
+﻿namespace DiscordRoleComparer
+{
+    /*
+     * Class that specifies a condition in which a Discord Role should be applied. Reads as below
+     * Apply "Role" if "Selected Rule" for "Value" is true.
+     * Example: Apply Donator if Patreon_Tier_Is Donor.
+    */
+    public class RoleRule
+    {
+        public string Role;
+
+        public Rules? SelectedRule;
+
+        public string Value;
+    }
+}
