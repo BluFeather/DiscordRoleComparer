9b1b3e3a743b1d7b76573007e8cea1f08563efdb
BluFeather <BluFeather@protonmail.com>
2022-11-06 09:17:57 -0800

    RuleRuleUiElement Dropdowns populated by DiscordMember and PatreonSubscriber Classes.

--- DiscordRoleComparer/ViewModel/RoleRuleUiElement.cs
+++ DiscordRoleComparer/ViewModel/RoleRuleUiElement.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace DiscordRoleComparer
@@ -12,51 +12,18 @@ namespace DiscordRoleComparer
         {
             Orientation = Orientation.Horizontal;
 
-            RoleRulesComboBox = new ComboBox() { ItemsSource = RoleRules, SelectedIndex = 0 };
+            RoleRulesComboBox = new ComboBox() { ItemsSource = DiscordMember.UniqueRoles, SelectedIndex = 0 };
             Children.Add(RoleRulesComboBox);
 
             RulesComboBox = new ComboBox() { ItemsSource = Enum.GetValues(typeof(Rules)), SelectedIndex = 0 };
             Children.Add(RulesComboBox);
 
-            TiersComboBox = new ComboBox() { ItemsSource = Tiers, SelectedIndex = 0 };
+            TiersComboBox = new ComboBox() { ItemsSource = PatreonSubscriber.UniqueTiers, SelectedIndex = 0 };
             Children.Add(TiersComboBox);
 
-            Children.Add(new Button() { Content = "Delete Rule" });
-        }
-
-        private RoleRule roleRule = new RoleRule();
-
-        private static HashSet<string> roleRules = new HashSet<string>();
-        public HashSet<string> RoleRules
-        {
-            get { return roleRules; }
-            set
-            {
-                roleRules = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Rules? selectedRules;
-        public Rules? SelectedRule
-        {
-            get { return selectedRules; }
-            set
-            {
-                selectedRules = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private static HashSet<string> tiers = new HashSet<string>();
-        public HashSet<string> Tiers
-        {
-            get { return tiers; }
-            set
-            {
-                tiers = value;
-                OnPropertyChanged();
-            }
+            DeleteRuleButton = new Button() { Content = "Delete Rule" };
+            DeleteRuleButton.Click += OnDeleteRuleButtonClicked;
+            Children.Add(DeleteRuleButton);
         }
 
         #region UI Elements
@@ -65,6 +32,17 @@ namespace DiscordRoleComparer
         ComboBox RulesComboBox;
 
         ComboBox TiersComboBox;
+
+        Button DeleteRuleButton;
+        #endregion
+
+        #region Events
+        public EventHandler DeleteRuleClicked;
+
+        private void OnDeleteRuleButtonClicked(object sender, RoutedEventArgs e)
+        {
+            DeleteRuleClicked?.Invoke(this, e);
+        }
         #endregion
 
         #region INotifyPropertyChanged Implementation
